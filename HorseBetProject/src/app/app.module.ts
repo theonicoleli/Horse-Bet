a0c9060641eda8a262470446deb2560efb2fc6a6
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2024-03-31 19:23:03 -0300

    Horse Bet Angular

---
+++ HorseBetProject/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MenuComponent } from './pages/menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
