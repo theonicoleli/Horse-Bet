e9cae856f283b07d699888d8d966b1a26a732b3a
Théo Nicoleli <118020861+theonicoleli@users.noreply.github.com>
2024-06-19 23:19:56 -0300

    Trabalho feito.

--- HorseBetProject/src/app/pages/historico/historico.component.ts
+++ HorseBetProject/src/app/pages/historico/historico.component.ts
@@ -1,63 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ApostaService } from '../lista/services/aposta.service';
-import { Observable } from 'rxjs/internal/Observable';
+import { Observable, Subscription } from 'rxjs';
 import { Aposta } from '../lista/model/aposta.model';
 import { AuthService } from '../../auth.service';
 import { BetService } from '../bet/service/bet.service';
 import { Bet } from '../bet/model/bet.model';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-historico',
   templateUrl: './historico.component.html',
   styleUrls: ['./historico.component.css']
 })
-export class HistoricoComponent {
-  apostas$!: Observable<Aposta[]>;
+export class HistoricoComponent implements OnInit, OnDestroy {
+  apostas$: Observable<Aposta[]>;
   betUserRaces: Aposta[] = [];
-  betUser: Observable<Bet[]> | undefined;
+  betUser: Bet[] = [];
   user: any;
+  private userSubscription: Subscription;
 
   constructor(
-    private apostaService: ApostaService, 
+    private apostaService: ApostaService,
     private authService: AuthService,
-    private betService: BetService,
-  ){}
-
-  ngOnInit() {
-    Promise.all([
-      this.authService.user$.subscribe(user => {
-        this.user = user;
-      }),
-      this.carregarCorridas(),
-    ]);
-  }
-
-  carregarCorridas(): void {
+    private betService: BetService
+  ) {
     this.apostas$ = this.apostaService.getApostas();
-    this.getBet();
+    this.userSubscription = new Subscription();
   }
 
-  getBet() {
-    this.betUser = this.betService.getApostas();
-    this.getBetRaces();
+  ngOnInit(): void {
+    this.userSubscription = this.authService.user$.pipe(
+      tap(user => this.user = user),
+      switchMap(user => {
+        if (user) {
+          return this.loadUserBets(user.email);
+        } else {
+          return [];
+        }
+      })
+    ).subscribe();
+  }
+
+  private loadUserBets(email: string): Observable<Bet[]> {
+    return this.betService.getApostas().pipe(
+      tap(bets => {
+        this.betUser = bets.filter(bet => bet.emailParticipante === email);
+        this.getBetRaces();
+      })
+    );
   }
 
   getBetRaces(): void {
     if (this.apostas$ && this.betUser) {
       this.apostas$.subscribe(apostas => {
         apostas.forEach(aposta => {
-          this.betUser!.subscribe(bets => { 
-            const bet = bets.find(bet => bet.nomeCorrida === aposta.nome);
-            if (bet) {
-              aposta.valorAposta = bet.valorAposta;
-              aposta.nomeCavalo = bet.nomeCavalo;
-              this.betUserRaces.push(aposta);
-            }
-          });
+          const bet = this.betUser.find(b => b.nomeCorrida === aposta.nome);
+          if (bet) {
+            aposta.valorAposta = bet.valorAposta;
+            aposta.nomeCavalo = bet.nomeCavalo;
+            this.betUserRaces.push(aposta);
+          }
         });
       });
     }
   }
-  
-}
 
+  ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
+  }
+}
